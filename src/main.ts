733dc87a27f855eb447c512958267b06e5cb3f05
Maksim Yersh <edelwud@icloud.com>
2023-03-16 01:52:30 +0300

    fix: provide aws account and region

--- src/main.ts
+++ src/main.ts
@@ -4,28 +4,18 @@ import { GithubActionsStack } from "./github-actions.stack";
 
 const app = PDKNag.app();
 
-const githubStack = new GithubActionsStack(app, "GithubActions", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+const env = {
+  account: "762714406455",
+  region: "us-east-1",
+};
 
-const exporterAppProd = new ExporterApplicationStage(app, "prod", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+const githubStack = new GithubActionsStack(app, "GithubActions", { env });
 
-const exporterAppDev = new ExporterApplicationStage(app, "dev", {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION,
-  },
-});
+const exporterAppProd = new ExporterApplicationStage(app, "prod", { env });
 
-githubStack.pipeline.addStage(exporterAppProd);
+const exporterAppDev = new ExporterApplicationStage(app, "dev", { env });
+
+githubStack.pipeline.addStageWithGitHubOptions(exporterAppProd, {});
 githubStack.pipeline.addStage(exporterAppDev);
 
 app.synth();
